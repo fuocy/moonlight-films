990e2daf9ab14cbacfcfe8eba01cdf64fd612ead
ph1109ji <ph1109ji@gmail.com>
2022-07-19 07:43:22 +0700

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0c1117",
+        "dark-lighten": "#24292e",
+        primary: "#5179ff",
+      },
+    },
+  },
+  plugins: [],
+};
