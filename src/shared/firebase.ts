692ed3e3b166bcabf52371ff33d9e69ab2ac3b0e
ph1109ji <ph1109ji@gmail.com>
2022-08-09 22:38:45 +0700

    BREAKING CHANGE: change firebase from moonlight to PVThouse

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -10,13 +10,24 @@ import { getFirestore } from "firebase/firestore";
 //   messagingSenderId: process.env.REACT_APP_messagingSenderId,
 //   appId: process.env.REACT_APP_appId,
 // });
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBiO4NTttc68zMHFkRFXyK5qLsU9zXsJmg",
+//   authDomain: "moonlight-40ecf.firebaseapp.com",
+//   projectId: "moonlight-40ecf",
+//   storageBucket: "moonlight-40ecf.appspot.com",
+//   messagingSenderId: "1043878072437",
+//   appId: "1:1043878072437:web:0fd78f6e64f3cfbe2d283c",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBiO4NTttc68zMHFkRFXyK5qLsU9zXsJmg",
-  authDomain: "moonlight-40ecf.firebaseapp.com",
-  projectId: "moonlight-40ecf",
-  storageBucket: "moonlight-40ecf.appspot.com",
-  messagingSenderId: "1043878072437",
-  appId: "1:1043878072437:web:0fd78f6e64f3cfbe2d283c",
+  apiKey: "AIzaSyC5V0Pm-YJV9h0kyX9aBgrKrs3_J3R2Foo",
+  authDomain: "pvt-comments.firebaseapp.com",
+  databaseURL: "https://pvt-comments-default-rtdb.firebaseio.com",
+  projectId: "pvt-comments",
+  storageBucket: "pvt-comments.appspot.com",
+  messagingSenderId: "700353567702",
+  appId: "1:700353567702:web:f36bbbae9c78ac1e3ea277",
 };
 const app = initializeApp(firebaseConfig);
 
