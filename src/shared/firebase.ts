fe8776570b889a1caa4f471abfec85c2e5ce33ed
ph1109ji <ph1109ji@gmail.com>
2022-08-10 16:36:41 +0700

    CHANGE: change back to use Moonlight firebase

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -11,24 +11,24 @@ import { getFirestore } from "firebase/firestore";
 //   appId: process.env.REACT_APP_appId,
 // });
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBiO4NTttc68zMHFkRFXyK5qLsU9zXsJmg",
-//   authDomain: "moonlight-40ecf.firebaseapp.com",
-//   projectId: "moonlight-40ecf",
-//   storageBucket: "moonlight-40ecf.appspot.com",
-//   messagingSenderId: "1043878072437",
-//   appId: "1:1043878072437:web:0fd78f6e64f3cfbe2d283c",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyC5V0Pm-YJV9h0kyX9aBgrKrs3_J3R2Foo",
-  authDomain: "pvt-comments.firebaseapp.com",
-  databaseURL: "https://pvt-comments-default-rtdb.firebaseio.com",
-  projectId: "pvt-comments",
-  storageBucket: "pvt-comments.appspot.com",
-  messagingSenderId: "700353567702",
-  appId: "1:700353567702:web:f36bbbae9c78ac1e3ea277",
+  apiKey: "AIzaSyBiO4NTttc68zMHFkRFXyK5qLsU9zXsJmg",
+  authDomain: "moonlight-40ecf.firebaseapp.com",
+  projectId: "moonlight-40ecf",
+  storageBucket: "moonlight-40ecf.appspot.com",
+  messagingSenderId: "1043878072437",
+  appId: "1:1043878072437:web:0fd78f6e64f3cfbe2d283c",
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC5V0Pm-YJV9h0kyX9aBgrKrs3_J3R2Foo",
+//   authDomain: "pvt-comments.firebaseapp.com",
+//   databaseURL: "https://pvt-comments-default-rtdb.firebaseio.com",
+//   projectId: "pvt-comments",
+//   storageBucket: "pvt-comments.appspot.com",
+//   messagingSenderId: "700353567702",
+//   appId: "1:700353567702:web:f36bbbae9c78ac1e3ea277",
+// };
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
